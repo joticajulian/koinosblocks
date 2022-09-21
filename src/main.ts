54361f1b389f68a8121d6ad80ce254c5f963961d
Bartłomiej Górnicki <bgor91@gmail.com>
2022-09-22 00:56:16 +0200

    feat: add page titles

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { createApp } from "vue"
+import {createApp} from "vue"
 import App from "./App.vue"
-import { createVuestic } from "vuestic-ui"
+import {createVuestic} from "vuestic-ui"
 import 'vuestic-ui/css'
 import Overview from './pages/Overview.vue';
 import Block from './pages/Block.vue';
@@ -10,13 +10,19 @@ import {createRouter, createWebHistory} from "vue-router";
 import hljsVuePlugin from "@highlightjs/vue-plugin";
 
 const routes = [
-    { path: '/', component: Overview },
-    { path: '/block/:height', component: Block },
-    { path: '/tx/:id', component: Transaction },
-    { path: '/address/:address', component: Address },
+    {path: '/', component: Overview},
+    {path: '/block/:height', component: Block, meta: {title: 'Koinosblocks.com - block details'}},
+    {path: '/tx/:id', component: Transaction, meta: {title: 'Koinosblocks.com - transaction details'}},
+    {path: '/address/:address', component: Address, meta: {title: 'Koinosblocks.com - address details'}},
 ]
 
 const router = createRouter({history: createWebHistory(), routes});
+
+router.beforeEach((to, from, next) => {
+    // @ts-ignore
+    document.title = to?.meta?.title ?? 'Koinosblocks.com - Koinos blockchain Explorer';
+    next();
+});
 
 const app = createApp(App)
 app.use(router)
