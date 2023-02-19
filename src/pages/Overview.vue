affb929495b743c2f8052b3a3f8fd00b07a67304
Bartłomiej Górnicki <bgor91@gmail.com>
2023-02-19 18:43:30 +0100

    chore: show coingecko marketcap widget

--- src/pages/Overview.vue
+++ src/pages/Overview.vue
@@ -1,9 +1,9 @@
 <template>
-<!--  <div class="row">-->
-<!--    <div class="offset&#45;&#45;sm row ma-3" style="width: 100%;">-->
-<!--      <MarketcapInfo />-->
-<!--    </div>-->
-<!--  </div>-->
+  <div class="row">
+    <div class="offset--sm row ma-3" style="width: 100%;">
+      <MarketcapInfo />
+    </div>
+  </div>
   <div class="row">
     <div class="offset--sm row ma-3 search-bar">
       <SearchBar />
@@ -26,8 +26,9 @@ import RecentBlocks from "../components/block/RecentBlocks.vue";
 import Navbar from "../components/layout/Navbar.vue";
 import HeadBlock from "../components/block/HeadBlock.vue";
 import SearchBar from "../components/layout/SearchBar.vue";
+import MarketcapInfo from "../components/common/MarketcapInfo.vue";
 export default {
-  components: {SearchBar, HeadBlock, Navbar, RecentBlocks},
+  components: {MarketcapInfo, SearchBar, HeadBlock, Navbar, RecentBlocks},
 }
 
 </script>
